d55a1bc24d8bc3e856e004cc884f86f0ba6cbf47
Gnomiostasis <catsarecows@gmail.com>
2017-10-14 10:31:30 -0700

    Minion Manager and Minion classes
    Added minion manager and minion classes and added references in
    index.html

---
+++ managers/minion/MinionManager.js
@@ -0,0 +1,15 @@
+﻿function MinionManager()
+{
+    this.minions = [];
+
+    function Update()
+    {
+        //Check if need to spawn
+        //check if need to die
+        //loop through each minion and update that bitch    
+        for (var minion in minions)
+        {
+            
+        }
+    }
+}
