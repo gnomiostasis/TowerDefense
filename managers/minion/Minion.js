eca41370013a5fef131755e7ec780e06559b35f4
Gnomiostasis <catsarecows@gmail.com>
2017-10-14 11:56:15 -0700

    Enhanced minionmanager/minions
    Added update method, moveup, movedown, moveleft, moveright.

--- managers/minion/Minion.js
+++ managers/minion/Minion.js
@@ -1,11 +1,44 @@
 ﻿function Minion() {
-    this.speed = 0.01;    //I don't know if this is an ok value
+    this.speed = 0.05;    //I don't know if this is an ok value
     this.health = 100;
-    this.color = [1, 1, 0]  //R, G, B     .... This is an arbitrary color
-    this.position = null;
+    this.color = new BABYLON.Color3(0, 1, 0);  //R, G, B , A?    .... This is an arbitrary color
+    this.gameobj = null;
+    this.material = null;
     this.direction = null;
-    this.Update() = function ()
-    {
-        //Move minions
-    }
+    
+}
+
+
+Minion.prototype.update = function () {
+
+}
+
+Minion.prototype.create = function () {
+    // Let's try our built-in 'sphere' shape. Params: name, subdivisions, size, scene
+    this.gameobj = BABYLON.Mesh.CreateSphere("sphere1_id", 16, 1, scene);
+    // Move the sphere upward 1/2 its height
+    this.gameobj.position.y = .5;
+    this.gameobj.position.x = 10;
+    this.gameobj.position.z = 10;
+    this.gameobj.material = new BABYLON.StandardMaterial("matPlan1", scene);
+    this.gameobj.material.emissiveColor = this.color;
+    this.gameobj.material.backFaceCulling = true;
+}
+
+Minion.prototype.destroy = function () {
+    //Probably jsut remove from renderer and stuff like that
+
+}
+
+Minion.prototype.moveUp = function () {
+    this.gameobj.position.z += this.speed;
+}
+Minion.prototype.moveDown = function () {
+    this.gameobj.position.z -= this.speed;
+}
+Minion.prototype.moveLeft = function () {
+    this.gameobj.position.x -= this.speed;
+}
+Minion.prototype.moveRight = function () {
+    this.gameobj.position.x += this.speed;
 }
--- managers/minion/MinionManager.js
+++ managers/minion/MinionManager.js
@@ -1,15 +1,18 @@
 ﻿function MinionManager()
 {
     this.minions = [];
+}
 
-    function Update()
-    {
+MinionManager.prototype.init = function () {
+    //var testmin = new Minion();
+    //testmin.create();
+    //this.minions.push(testmin);
+}
+MinionManager.prototype.update = function () {
+    //loop through each minion and update that bitch    
+    for (var minion in this.minions) {
         //Check if need to spawn
         //check if need to die
-        //loop through each minion and update that bitch    
-        for (var minion in minions)
-        {
-            
-        }
+        this.minions[minion].update();
     }
 }
