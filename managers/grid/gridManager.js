f7b598183c26a94af28788a00c12a3cd9ec77eb1
DuffyStudio <christopher.b.duffy@gmail.com>
2017-10-14 14:24:13 -0700

    color

--- managers/grid/gridManager.js
+++ managers/grid/gridManager.js
@@ -3,16 +3,25 @@ function initGridManager()
 	this.grid = createGrid();
 	
 	
-	function createCursor(){
+	function createCursor(mirror){
 		var cursor = {
-			x:0,
-			z:0
+			x:-14.5,
+			z:9.5*mirror,
+			maxZ: mirror > 0 ? 9.5 : -1.5,
+			minZ: mirror >0 ? 1.5 : -9.5
 		}
 		
 		cursor.gameobject = BABYLON.Mesh.CreateBox("plane", 1, scene);
-		cursor.gameobject.position.y = .2;
-		//cursor.gameobject.position.x=cursor.x;
-		//cursor.gameobject.position.z = cursor.z;
+		cursor.gameobject.position.y = -.5;
+		cursor.gameobject.position.x=cursor.x;
+		cursor.gameobject.position.z = cursor.z;
+		cursor.gameobject.material = new BABYLON.StandardMaterial("matPlan1", scene);
+		if (mirror>0){
+			cursor.gameobject.material.emissiveColor = new BABYLON.Color4(1, 0, 0,.2);
+		}else{
+			cursor.gameobject.material.emissiveColor = new BABYLON.Color4(0, 0, 1,.2);	
+		}
+	
 		cursor.getSelf = function() {
 			return this;
 		}
@@ -20,9 +29,9 @@ function initGridManager()
 		cursor.moveLeft= function(){
 			cursor.x -=1;
 			cursor.gameobject.position.x-=1;
-			if(cursor.x<-15){
-				cursor.x = -15;
-				//cursor.gameobject.position.x=cursor.x;
+			if(cursor.x<-14.5){
+				cursor.x = -14.5;
+				cursor.gameobject.position.x=cursor.x;
 			}
 			
 		}
@@ -30,28 +39,48 @@ function initGridManager()
 		cursor.moveRight= function(){
 			cursor.x +=1;
 			cursor.gameobject.position.x+=1;
-			if(cursor.x>15){
-				cursor.x = 15;
-				//cursor.gameobject.position.x=cursor.x;
+			if(cursor.x>14.5){
+				cursor.x = 14.5;
+				cursor.gameobject.position.x=cursor.x;
 			}
 			
 		}
 		
 		cursor.moveDown= function(){
+			cursor.z = Math.max(cursor.z - 1, cursor.minZ);
+			cursor.gameobject.position.z = cursor.z;
+			return;
+			
 			cursor.z -=1;
 			cursor.gameobject.position.z-=1;
-			if(cursor.z<-10){
-				cursor.z = -10;
-				//cursor.gameobject.position.z = cursor.z;
+			if(mirror>0){ 
+				if(cursor.z<1.5){
+					cursor.z = 1.5;
+					cursor.gameobject.position.z = cursor.z;
+				}
+				if(cursor.z<-9.5){
+					cursor.z = -9.5;
+					cursor.gameobject.position.z = cursor.z;
+				}
 			}
 		}
 		
 		cursor.moveUp= function(){
+			cursor.z = Math.min(cursor.z + 1, cursor.maxZ);
+			cursor.gameobject.position.z = cursor.z;
+			return;
+			
 			cursor.z +=1;
 			cursor.gameobject.position.z+=1;
-			if(cursor.z>10){
-				cursor.z = 10;
-				//cursor.gameobject.position.z = cursor.z;
+			if(mirror>0){ 
+				if(cursor.z>9.5){
+					cursor.z = 9.5;
+					cursor.gameobject.position.z = cursor.z;
+				}
+				if(cursor.z>-1.5){
+					cursor.z = -1.5;
+					cursor.gameobject.position.z = cursor.z;
+				}
 			}
 		}
 		
@@ -81,8 +110,8 @@ function initGridManager()
 		
 		grid.update = function(){}
 		
-		grid.cursor1 = createCursor();
-		grid.cursor2 = createCursor();
+		grid.cursor1 = createCursor(1);
+		grid.cursor2 = createCursor(-1);
 		return grid;
 		
 		
